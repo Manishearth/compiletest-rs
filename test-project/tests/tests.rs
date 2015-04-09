0a79563fce41b816f33785d451e4ade84f3baf8d
Thomas Jespersen <laumann.thomas@gmail.com>
2015-04-09 12:45:25 +0200

    Remove unnecessary cruft from test project

--- test-project/tests/tests.rs
+++ test-project/tests/tests.rs
@@ -1,34 +1,6 @@
 extern crate compiletest;
 
-use std::env;
-use std::process::Command;
 use std::path::PathBuf;
-
-#[test]
-fn test_invoke() {
-    // note that there are a number of downsides to this approach, the comments
-    // below detail how to improve the portability of these commands.
-    Command::new("echo").args(&["Hello, ", "World!"])
-        .status().unwrap();
-}
-
-
-#[test]
-fn have_args() {
-    let args: Vec<String> = env::args().collect();
-
-    Command::new("echo").arg(format!("{:?}", args))
-        .status().unwrap();
-
-    assert!(args.len() > 0);
-}
-
-static LD_LIBRARY_PATH: &'static str = env!("LD_LIBRARY_PATH");
-
-#[test]
-fn print_ld_library_path() {
-    Command::new("echo").arg(format!("{:?}", LD_LIBRARY_PATH)).status().unwrap();
-}
 
 fn run_mode(mode: &'static str) {
 
