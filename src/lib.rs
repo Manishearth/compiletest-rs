961849b2e7cb16126d6e604e347d5699b43f1a3f
Thomas Jespersen <laumann.thomas@gmail.com>
2018-10-28 13:55:55 +0100

    Map config.run_ignored to test::RunIgnored

--- src/lib.rs
+++ src/lib.rs
@@ -98,7 +98,7 @@ pub fn test_opts(config: &Config) -> test::TestOpts {
     test::TestOpts {
         filter: config.filter.clone(),
         filter_exact: config.filter_exact,
-        run_ignored: config.run_ignored,
+        run_ignored: if config.run_ignored { test::RunIgnored::Yes } else { test::RunIgnored::No },
         format: if config.quiet { test::OutputFormat::Terse } else { test::OutputFormat::Pretty },
         logfile: config.logfile.clone(),
         run_tests: true,
