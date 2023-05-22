f9f621599c032e096def4d01f02b8afdcd3a35e1
Thom Chiovoloni <thom@shift.click>
2023-05-22 09:39:37 -0700

    Use a different name for the host/target env vars

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 
 pub fn main() {
     if env::var("CARGO_FEATURE_RUSTC").is_err() {
-        println!("cargo:rustc-env=TARGET={}", env::var("TARGET").unwrap());
-        println!("cargo:rustc-env=HOST={}", env::var("HOST").unwrap());
+        println!("cargo:rustc-env=COMPILETEST_TARGET={}", env::var("TARGET").unwrap());
+        println!("cargo:rustc-env=COMPILETEST_HOST={}", env::var("HOST").unwrap());
     }
 }
